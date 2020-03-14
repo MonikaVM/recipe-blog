6b0598a8e6c322a8afc0aa55a534823596eb6516
Mewmew <vmonika028@gmail.com>
2020-03-14 14:55:51 +0100

    refactor routes

--- app.js
+++ app.js
@@ -1,105 +1,23 @@
 const express        = require('express'),
       app            = express(),
-      cors           = require('cors')
       mongoose       = require('mongoose'),
       bodyParser     = require('body-parser'),
       methodOverride = require('method-override'),
       Recipe         = require('./models/recipe')
+
+const indexRoutes = require('./routes/index')
+const recipeRoutes = require('./routes/recipes')
 
 // app setup
 mongoose.connect('mongodb://localhost:27017/recipe_blog', {useNewUrlParser: true, useUnifiedTopology: true})
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))
 app.use(methodOverride("_method"))
-app.use(cors())
 app.set('view engine', 'ejs')
-app.get('/', (req, res) => {
-    res.redirect('/blog')
-})
 
-// shows all recipes
-app.get('/blog', (req, res) => {
-    Recipe.find({}, (err, recipes) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.render('blog', {recipes})
-        }
-    })
-})
-
-// shows form for new recipe
-app.get('/blog/new', (req, res) => {
-    res.render('new')
-})
-
-// shows particular recipe
-app.get('/blog/:id', (req, res) => {
-    Recipe.findById(req.params.id, (err, recipe) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.render('show', {recipe})
-        }
-    })
-})
-
-// creates new recipe and adds it to DB
-app.post('/blog', (req, res) => {
-    Recipe.create(req.body.blog, (err, newlyCreated) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log('Recipe successfully added!')
-            console.log(newlyCreated)
-            res.redirect('/blog')
-        }
-    })
-})
-
-// edit recipe
-// shows form to edit recipe
-app.get('/blog/:id/edit', (req, res) => {
-    Recipe.findById(req.params.id, (err, foundRecipe) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.render('edit', {foundRecipe})
-        }
-    })
-})
-
-// update recipe
-app.put('/blog/:id', (req, res) => {
-    const updatedRecipe = {
-        name: req.body.name,
-        image: req.body.image,
-        ingredients: [...req.body.ingredients || ''],
-        description: req.body.description
-    }
-    Recipe.findByIdAndUpdate(req.params.id, updatedRecipe, (err, updated) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.redirect(`/blog/${req.params.id}`)
-        }
-    })
-})
-
-// deletes recipe
-app.delete('/blog/:id', (req, res) => {
-    Recipe.findByIdAndRemove(req.params.id, (err, deletedRecipe) => {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(`Deleted: ${deletedRecipe}`)
-        }
-        res.redirect('/blog')
-    })
-})
+app.use('/', indexRoutes)
+app.use('/blog', recipeRoutes)
 
 app.listen(3000, () => {
     console.log('Server started!')
 })
-
-
---
+++ routes/recipes.js
@@ -0,0 +1,86 @@
+const express = require('express'),
+      Recipe = require('../models/recipe'),
+      router = express.Router()
+
+// shows all recipes
+router.get('/', (req, res) => {
+    Recipe.find({}, (err, recipes) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('blog', {recipes})
+        }
+    })
+})
+
+// shows form for new recipe
+router.get('/new', (req, res) => {
+    res.render('new')
+})
+
+// shows particular recipe
+router.get('/:id', (req, res) => {
+    Recipe.findById(req.params.id, (err, recipe) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('show', {recipe})
+        }
+    })
+})
+
+// creates new recipe and adds it to DB
+router.post('/', (req, res) => {
+    Recipe.create(req.body.blog, (err, newlyCreated) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log('Recipe successfully added!')
+            console.log(newlyCreated)
+            res.redirect('/blog')
+        }
+    })
+})
+
+// edit recipe
+// shows form to edit recipe
+router.get('/:id/edit', (req, res) => {
+    Recipe.findById(req.params.id, (err, foundRecipe) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.render('edit', {foundRecipe})
+        }
+    })
+})
+
+// update recipe
+router.put('/:id', (req, res) => {
+    const updatedRecipe = {
+        name: req.body.name,
+        image: req.body.image,
+        ingredients: [...req.body.ingredients || ''],
+        description: req.body.description
+    }
+    Recipe.findByIdAndUpdate(req.params.id, updatedRecipe, (err, updated) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.redirect(`/blog/${req.params.id}`)
+        }
+    })
+})
+
+// deletes recipe
+router.delete('/:id', (req, res) => {
+    Recipe.findByIdAndRemove(req.params.id, (err, deletedRecipe) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(`Deleted: ${deletedRecipe}`)
+        }
+        res.redirect('/blog')
+    })
+})
+
+module.exports = router
